751bf82216b2a7272d71cf7179a23a0f77b1833f
leh-torres <leticiatp2@gmail.com>
2023-10-23 21:11:03 -0300

    Grafico 6 finalizado

--- javascript/traducao/i18n-dashboard.js
+++ javascript/traducao/i18n-dashboard.js
@@ -88,6 +88,11 @@ const lngs = {
                   ajudaPorModeloDeReceita: "Esse gráfico mostra a quantidade do modelo de receita escolhido (pelo filtro “Escolha o modelo de receita”) nas fases de startups (se estiver em dúvida de quais startups são, saiba que são todas que possuem sua fase cadastrada). Os dados são obtidos pelos registros efetuados no mapa do ecossistema de inovação cearense. O gráfico abaixo ainda está em uma versão com dados preliminares. Portanto, caso você seja uma startup e deseja contribuir conosco compartilhando os dados da sua startup, por favor entre em contato por email informando o segmento, fase e modelo de negócio.",
                 
                 },
+                grafico6:{
+                  titulo: "Segmentos de Startup",
+                  todas: "Todas as regiões",
+                  ajuda: "Esse gráfico mostra os segmentos de startup e a quantidade de startup em cada um deles nas regiões ou comunidades do Ceará, sendo possível escolher uma região ou comunidade específica através do filtro disponível. Os dados são obtidos pelos registros efetuados no mapa do ecossistema de inovação cearense."
+                },
                 categorias:{
                   comunidades: 'Comunidades',
                   aceleradora: 'Aceleradora',
@@ -190,6 +195,11 @@ const lngs = {
                     ajudaPorSegmento: "This graph shows the number of existing startup segments (it is possible to choose which segment to view using the “Choose the segment” filter) in the phases of those startups (if you are in doubt which startups they are, know that they are all that have their phase registered in the segment chosen). The data is obtained from records made on the Mapa do Ecossitema de Inovação Cearense. The graph below is still in a version with preliminary data. Therefore, if you are a startup and wish to contribute to us by sharing your startup's data, please contact us by email informing the segment, phase and business model.",
                     ajudaPorModeloDeReceita: "This graph shows the number of revenue models chosen (through the “Choose revenue model” filter) in the startup phases (if you are in doubt which startups they are, know that they all have their phase registered). The data is obtained from records made on the Mapa do Ecossistema de Inovação Cearense. The graph below is still in a version with preliminary data. Therefore, if you are a startup and wish to contribute to us by sharing your startup's data, please contact us by email informing the segment, phase and business model.",
   
+                  },
+                  grafico6:{
+                    titulo: "Startup Segments",
+                    todas: "All regions",
+                    ajuda: "This graph shows the startup segments and the number of startups in each of them in the regions or communities of Ceará, making it possible to choose a specific region or community using the available filter. The data is obtained from records made on the Mapa do Ecossitema de Inovação do Ceará."
                   },
                   categorias:{
                     comunidades: 'Communities',
